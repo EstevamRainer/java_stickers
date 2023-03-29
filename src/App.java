73bedc71d3811e1063e91fe90b95e52be99993ae
Estevam Rainer <estevamrainerdev@gmail,com>
2023-03-29 10:08:16 -0300

    Além de consumir a API do ImDb, o app.java agora cria figurinhas com as imagens dos posteres dos filmes!

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -18,6 +20,12 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         for (var i = 0; i < 10; i++) {
             Map<String,String> filme = listaDeFilmes.get(i);
+            String imagemUrl = filme.get("image");
+            InputStream inputStream = new URL(imagemUrl).openStream();
+            String titulo = filme.get("title");
+            String nomeArquivo = titulo + ".png";
+            var geradora = new geradorDeFirugrinhas();
+            geradora.cria(inputStream, nomeArquivo);
             System.out.println("🤳 \u001b[1m \u001b[33m \u001b[44m Poster:\u001b[m " + filme.get("image"));
             System.out.println("📖 \u001b[1m \u001b[33m \u001b[41m Título:\u001b[m " + filme.get("title"));
             System.out.println("🌟 \u001b[1m \u001b[33m \u001b[43m Nota:\u001b[m " + filme.get("imDbRating"));
